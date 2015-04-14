d7b7e30a1f7f5bb9173f32bde1b1ede6de32254d
Jake Bolewski <jakebolewski@gmail.com>
2015-04-14 12:37:20 -0700

    add zeros! function

--- src/matrices.jl
+++ src/matrices.jl
@@ -3,30 +3,36 @@ for (elty, relty, ext) in ((:Float32, :Float32, :s),
                            (:Complex64, :Float32, :c),
                            (:Complex128, :Float64, :z))
 
-    # Gaussian
     for (mat, sym) in ((:Matrix, "_"),
                        (:DistMatrix, "Dist_"),
                        (:DistMultiVec, "DistMultiVec_"))
         @eval begin
-            function gaussian!(A::$mat{$elty}, m::Integer, n::Integer, mean::$elty = zero($elty), stddev::$relty = one($relty))
+
+            # Gaussian
+            function gaussian!(A::$mat{$elty}, m::Integer, n::Integer,
+                               mean::$elty=zero($elty), stddev::$relty=one($relty))
                 err = ccall(($(string("ElGaussian", sym, ext)), libEl), Cuint,
                     (Ptr{Void}, ElInt, ElInt, $elty, $relty),
                     A.obj, m, n, mean, stddev)
                 err == 0 || throw(ElError(err))
                 return A
             end
-        end
-    end
 
-    # Uniform
-    for (mat, sym) in ((:Matrix, "_"),
-                       (:DistMatrix, "Dist_"),
-                       (:DistMultiVec, "DistMultiVec_"))
-        @eval begin
-            function uniform!(A::$mat{$elty}, m::Integer, n::Integer, center::$elty = zero($elty), radius::$relty = one($relty))
+            # Uniform
+            function uniform!(A::$mat{$elty}, m::Integer, n::Integer,
+                              center::$elty=zero($elty), radius::$relty=one($relty))
                 err = ccall(($(string("ElUniform", sym, ext)), libEl), Cuint,
                     (Ptr{Void}, ElInt, ElInt, $elty, $relty),
                     A.obj, m, n, center, radius)
+                err == 0 || throw(ElError(err))
+                return A
+            end
+
+            # Zeros
+            function zeros!(A::$mat{$elty}, m::Integer, n::Integer)
+                err = ccall(($(string("ElZeros", sym, ext)), libEl), Cuint,
+                    (Ptr{Void}, ElInt, ElInt),
+                    A.obj, m, n)
                 err == 0 || throw(ElError(err))
                 return A
             end
